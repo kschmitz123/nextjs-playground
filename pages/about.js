1826baa793480b2ddb232e42b87b6fcc2d67fcbc
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-25 11:54:31 +0100

    Pages and routing

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ad
+        corporis modi deleniti praesentium. Ratione deserunt sequi cupiditate
+        eius perferendis expedita amet quis, ducimus laborum, consequuntur
+        corrupti, praesentium ipsum tempora.
+      </p>
+    </div>
+  );
+};
+
+export default About;
