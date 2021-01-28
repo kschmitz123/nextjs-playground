0054599a14f9fc9acf8d5a011d0bf9ce6615604b
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-28 12:25:53 +0100

    Add other components

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import Head from "next/head";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
+      <Navbar />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero numquam
         reprehenderit officia nobis eveniet molestias? Nobis aliquam, saepe
         magni magnam inventore commodi, beatae voluptatibus dignissimos
         provident, harum reprehenderit corrupti debitis.
       </p>
+      <Footer />
     </div>
   );
 }
